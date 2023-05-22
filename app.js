dcc9f7fefa1c087ff572206435e60b438c0d26d9
Gaurav Rathore <63956933+thatsleepytalker@users.noreply.github.com>
2023-05-22 12:33:26 +0530

    basic

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
 const express = require("express");
 const path = require('path');
 const ejs = require('ejs');
+const bodyParser = require('body-parser');
 
 const app = express()
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
 app.set('view engine', 'ejs')
+
+var data = []
 
 app.get('/', (req, res) => {
     res.render('index')
 })
 
 app.get('/login', (req,res) => {
-    res.send(req.query)
+    const username = req.body.username;
+    const password = req.body.password;
+    for(let i = 0; i < data.length; i++){
+        if (data[i]['username'] == username && data[i]['password'] == password) {
+                res.render('loginSuccess');
+        }
+    }
+    res.render('loginFailure')
 }) 
 
 app.get('/signup', (req,res) => {
     res.render('signup')
+})
+
+app.post('/signup', (req, res) => {
+    data.push(req.body)
+    console.log(data)
+    res.render('afterSignUp')
 })
 
 app.listen(3010, () => {
