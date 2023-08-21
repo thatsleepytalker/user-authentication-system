dfa5c99af40b4492e1300592ba81978526a40000
Gaurav Rathore <63956933+thatsleepytalker@users.noreply.github.com>
2023-08-22 03:52:29 +0530

    mongo db cloud connected

--- app.js
+++ app.js
@@ -2,8 +2,32 @@ const express = require("express");
 const path = require('path');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const app = express()
+const uri = process.env.CONNECTIONSTRING;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+const app = express();
+client.connect(err => {
+    if(err) {
+      console.error("Connection error", err);
+      return;
+    }
+  
+    const collection = client.db("UserAuthenticationSystem").collection("Users");
+    // perform actions on the collection object
+    client.close();
+  });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to MongoDB');
+});
+
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')));
@@ -11,9 +35,20 @@ app.use(bodyParser.urlencoded({
     extended:true
 }));
 
+
+
 app.set('view engine', 'ejs')
 
+// Schema and Data's
 var data = []
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    username: String,
+    email: String,
+    password: String
+  });
+const User = mongoose.model('User', userSchema);
+
 
 app.get('/', (req, res) => {
     res.render('index')
@@ -35,8 +70,13 @@ app.get('/signup', (req,res) => {
 })
 
 app.post('/signup', (req, res) => {
-    data.push(req.body)
-    console.log(data)
+    const newUser = new User({
+        username: req.body.Username,
+        email: req.body.Email,
+        password: req.body.Password
+    });
+    
+    newUser.save();
     res.render('afterSignUp')
 })
 
