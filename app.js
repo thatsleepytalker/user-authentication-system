0cb32be79d0ead02d11872d879c6dc5d778a1fc7
Gaurav Rathore <63956933+thatsleepytalker@users.noreply.github.com>
2023-08-22 17:48:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,20 +6,20 @@ const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const app = express();
+
+// ENV Variables
+const port = process.env.PORT;
 const uri = process.env.CONNECTIONSTRING;
+
+// Connection to MongoDB Cloud
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-
-const app = express();
 client.connect(err => {
     if(err) {
       console.error("Connection error", err);
       return;
     }
-  
-    const collection = client.db("UserAuthenticationSystem").collection("Users");
-    // perform actions on the collection object
     client.close();
   });
 const db = mongoose.connection;
@@ -27,20 +27,7 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('Connected to MongoDB');
 });
-
-
-app.use(express.json())
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({
-    extended:true
-}));
-
-
-
-app.set('view engine', 'ejs')
-
 // Schema and Data's
-var data = []
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
     username: String,
@@ -48,38 +35,59 @@ const userSchema = new Schema({
     password: String
   });
 const User = mongoose.model('User', userSchema);
+// Collection Variable to perform actions on collection
+const collection = client.db("UserAuthenticationSystem").collection("Users");
 
+// body parser use
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+
+// view engine set to EJS
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.render('index')
 })
 
+// Login Page Setup
 app.get('/login', (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
-    for(let i = 0; i < data.length; i++){
-        if (data[i]['username'] == username && data[i]['password'] == password) {
-                res.render('loginSuccess');
-        }
+    console.log(collection.findOne({username: username, password: password}));
+    async function fetchUser(users, passs){
+        const user = await collection.findOne({username: users});
+        console.log(user);
     }
-    res.render('loginFailure')
+    fetchUser(username, password);
+
+    /*if (){
+        res.render('loginSuccess');
+    }else{
+        res.render('loginFailure');
+    };*/
 }) 
 
+// Signup Page Render
 app.get('/signup', (req,res) => {
     res.render('signup')
 })
 
+// Posting new user Signup Page Backend
 app.post('/signup', (req, res) => {
     const newUser = new User({
         username: req.body.Username,
         email: req.body.Email,
         password: req.body.Password
     });
-    
     newUser.save();
     res.render('afterSignUp')
 })
 
-app.listen(3010, () => {
-    console.log('listening on port 3010!')
+// Starting Server, Listening to Port 
+app.listen(port, () => {
+    console.log(`Listening on Port: ${port}`)
 })
